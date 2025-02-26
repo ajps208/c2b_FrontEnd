9e7d553d6a17dacb11230d0e82ea000a1aeabe83
ajps208 <ajithps208@gmail.com>
2025-02-26 15:22:54 +0530

    5

--- src/USER/dashboard/UserDashboard.jsx
+++ src/USER/dashboard/UserDashboard.jsx
@@ -5,6 +5,7 @@ import Navbar from '../../COMMON/NavBar'
 import Tabs from '@/COMMON/Tabs';
 import { Grid } from '@mui/material';
 import Shops from '../Shops/Shops';
+import Dashboard1 from './DashBoard';
 
 export const UserDashboard = () => {
     const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -14,7 +15,7 @@ export const UserDashboard = () => {
 
       const tabsData = [
         { label: 'Shops', value: '1', content: <Shops></Shops>},
-        { label: 'DashBoard', value: '2', content: 'This is the content of Item Two' },
+        { label: 'DashBoard', value: '2', content:<Dashboard1></Dashboard1> },
       ];
   return (
     <>
